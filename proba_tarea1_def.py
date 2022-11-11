b8030bb9afbe42b05832cdee25d1366e532d187e
Loreto <82163134+loretito@users.noreply.github.com>
2022-11-11 12:35:16 -0300

    Update proba_tarea1_def.py

--- proba_tarea1_def.py
+++ proba_tarea1_def.py
@@ -12,12 +12,19 @@ datos = []
 for i in range(0, 10000): 
     datos.append(sumaDados())  #se guardan los 10000 resultados
 
-
-data = {}                   #se crea un diccionario con las llaves de 10 a 60 y con valor 0 
+#se crean dos diccionarios con las llaves de 10 a 60 y con valor 0
+# uno de ellos para realizar el calculo de la frecuencia y el otro para el calculo de la esperanza
+data = {}    
+newData = {}
 for i in range(10,61):
     data[i] = 0
+    newData[i] = 0
 
-def frec_abs(datos):  #esta funcion va sumando cuantas veces se repite x resultado (frecuencia absoluta) y lo guarda como valor en el diccionario anterior
+
+#esta funcion va sumando cuantas veces se repite x resultado 
+#(frecuencia absoluta) y lo guarda como valor en el diccionario anterior
+
+def frec_abs(datos):  
     for i in datos:
         if (i in data):   
             data[i] += 1 
@@ -38,13 +45,42 @@ plt.show()
 
 #ahora calculamos la esperanza de cada valor 
 
-esperanza = [] #un arreglo vacio 
+esperanza = [] #un arreglo vacio, donde se almacenaran las probabilidades de cada valor
+
+
+for a in range(1, 7):  #se hacen 10 for que le dan valores a los dados para calcular las combinaciones posibles de suma
+    d1 = a 
+    for b in range(1, 7):
+        d2 = b 
+        for c in range(1, 7):
+            d3 = c 
+            for d in range(1, 7):
+                d4 = d 
+                for e in range(1, 7):
+                    d5= e 
+                    for f in range(1, 7):
+                        d6 = f 
+                        for g in range(1, 7):
+                            d7 = g
+                            for h in range(1, 7):
+                                d8 = h 
+                                for i in range(1, 7):
+                                    d9 = i 
+                                    for j in range(1, 7):
+                                        d10 = j;
+                                        suma = d1 + d2 + d3 + d4 + d5 + d6 + d7 + d8 + d9 + d10 
+                                        newData[suma] += 1   #se va agregando la combinacion al posible resultado 
+                                        
+y1 = list(newData.values()) #se guardan las combinaciones posibles
 
 for i in range(0, len(x)):  
-    esperanza.append((y[i]/10000)*x[i])    #se recorre el array de x e y, y se divide por 10000 para obtener la probabilidad y se multiplica por x
+    esperanza.append((y1[i]/60466176)*x[i]) #se calcula la esperanza de cada valor, y se divide por el total de combinaciones posibles y se multiplica por el x 
 
 plt.plot(x,esperanza)
 plt.xlabel('x')
 plt.ylabel('Esperanza') 
 plt.title('Esperanza de cada valor')
 plt.show()
+
+
+
